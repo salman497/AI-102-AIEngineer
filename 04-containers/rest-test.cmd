3ad2cb1076d884a2509eeaddee241d8758eb2a03
Salman Aziz <salman.aziz@nintex.com>
2022-10-11 09:29:12 +1100

    lab 4

--- 04-containers/rest-test.cmd
+++ 04-containers/rest-test.cmd
@@ -1,1 +1,1 @@
-curl -X POST "http://<your_ACI_IP_address_or_FQDN>:5000/text/analytics/v3.0/languages?" -H "Content-Type: application/json" --data-ascii "{'documents':[{'id':1,'text':'Hello world.'},{'id':2,'text':'Salut tout le monde.'}]}"
+curl -X POST "http://salmanlab4.a6hbgsg3b0ccehgg.eastus.azurecontainer.io:5000/text/analytics/v3.0/languages?" -H "Content-Type: application/json" --data-ascii "{'documents':[{'id':1,'text':'Hello world.'},{'id':2,'text':'Salut tout le monde.'}]}"
