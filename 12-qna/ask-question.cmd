7cce49c901a3e30aeb0fba999c415eee1ba0fe6a
Salman Aziz <salman.aziz@nintex.com>
2022-10-12 10:34:10 +1100

    lab 12

--- 12-qna/ask-question.cmd
+++ 12-qna/ask-question.cmd
@@ -2,7 +2,7 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 rem Set variables
-set prediction_url="YOUR_PREDICTION_ENDPOINT"
-set key="YOUR_KEY"
+set prediction_url="https://salmanlab12.cognitiveservices.azure.com/language/:query-knowledgebases?projectName=LearnFAQ&api-version=2021-10-01&deploymentName=test"
+set key="cb772edd00bf4b92a1f22d4f9842cc46"
 
 curl -X POST !prediction_url! -H "Ocp-Apim-Subscription-Key: !key!" -H "Content-Type: application/json" -d "{'question': 'What is a learning Path?' }"
