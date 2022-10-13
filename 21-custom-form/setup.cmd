c2a83007f34bf6ed6e8aac8a3d7e9c8ef26003c0
Salman Aziz <salman.aziz@nintex.com>
2022-10-13 14:44:54 +1100

    lab 21

--- 21-custom-form/setup.cmd
+++ 21-custom-form/setup.cmd
@@ -2,9 +2,9 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 rem Set variable values 
-set subscription_id=YOUR_SUBSCRIPTION_ID
-set resource_group=YOUR_RESOURCE_GROUP
-set location=YOUR_LOCATION_NAME
+set subscription_id=f82471ae-8f97-493a-88fe-7b72f7e502f4
+set resource_group=ResourceGroup1lod26278181
+set location=eastus
 set expiry_date=2023-01-01T00:00:00Z
 
 rem Get random numbers to create unique resource names
